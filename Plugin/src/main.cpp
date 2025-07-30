823365e445e3838942f67d58d4a590ef2f4d6066
2084-debug <melik-yalcin@hotmail.com>
2025-07-30 10:41:18 +0300

    Initialize sfse

---
+++ Plugin/src/main.cpp
@@ -0,0 +1,66 @@
+#include "SFSE/Stub.h"
+
+DLLEXPORT constinit auto SFSEPlugin_Version = []() noexcept {
+	SFSE::PluginVersionData data{};
+
+	data.PluginVersion(Plugin::Version);
+	data.PluginName(Plugin::NAME);
+	data.AuthorName(Plugin::AUTHOR);
+
+	// Address Library v1 (https://www.nexusmods.com/starfield/mods/3256)
+	//data.UsesAddressLibrary(true);
+	// Version independent signature scanning
+	//data.UsesSigScanning(true);
+
+	// Uses version specific structure definitions
+	//data.IsLayoutDependent(true);
+	//data.HasNoStructUse(true);
+
+	data.CompatibleVersions({ RUNTIME_VERSION_1_7_33 });
+
+	return data;
+}();
+
+namespace
+{
+	void MessageCallback(SFSE::MessagingInterface::Message* a_msg) noexcept
+	{
+		switch (a_msg->type) {
+		case SFSE::MessagingInterface::kPostLoad:
+			{
+				break;
+			}
+		default:
+			break;
+		}
+	}
+}
+
+/**
+// for preload plugins
+void SFSEPlugin_Preload(SFSE::LoadInterface* a_sfse);
+/**/
+
+DLLEXPORT bool SFSEAPI SFSEPlugin_Load(SFSEInterface* a_sfse)
+{
+#ifndef NDEBUG
+	while (!IsDebuggerPresent()) {
+		Sleep(100);
+	}
+#endif
+
+	SFSE::Init(a_sfse);
+
+	DKUtil::Logger::Init(Plugin::NAME, std::to_string(Plugin::Version));
+
+	INFO("{} v{} loaded", Plugin::NAME, Plugin::Version);
+
+	// do stuff
+	// this allocates 1024 bytes for development builds, you can
+	// adjust the value accordingly with the log result for release builds
+	//SFSE::AllocTrampoline(1 << 10);
+
+	SFSE::GetMessagingInterface()->RegisterListener(MessageCallback);
+
+	return true;
+}
